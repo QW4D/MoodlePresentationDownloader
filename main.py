86ac0dcf5276f62ac02ffd195ca5b0480f71daef
QW4D <63963767+QW4D@users.noreply.github.com>
2023-12-25 00:15:00 +0300

    added support to MoodleSession for presentations that can only be viewed when logged in

--- main.py
+++ main.py
@@ -3,23 +3,33 @@ from img2pdf import convert
 import shutil
 import os
 
-link = input("ссылка на слайд (номер слайда заменить на {}):")
+link = input("ссылка на слайд (номер слайда заменить на {}): ")
 
-i = 1
+if input("тип:\n [0] презентация\n [1] лекция\n") == "1":
+    type = 1
+else:
+    type = 0
+i = int(type)
+
+s = requests.session()
+cookies = {"MoodleSession": input("moodleSession куки (оставить пустым, если не нужно): ")}
+if "tmp" in os.listdir():
+    shutil.rmtree("tmp")
 os.mkdir("tmp")
-while requests.get(link.format(i)).ok:
-    response = requests.get(link.format(i), stream=True)
+while requests.get(link.format(i), cookies=cookies).ok:
+    response = requests.get(link.format(i), stream=True, cookies=cookies)
     with open(f'tmp/{i}.png', 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
-
+    print(i)
     i += 1
 
-i = 64
 n = i
 imgs = []
 for i in range(n):
-    imgs.append(f'tmp/{i+1}.png')
+    if i != (not type):
+        imgs.append(f'tmp/{i}.png')
+
 print(imgs)
 with open('presentation.pdf', "wb") as pdf:
     pdf.write(convert(imgs))
